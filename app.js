54cb98ee7522a5c8954208d5b577c7dc809e692c
Manuel_Rodriguez <manuelrodriguez@correo.unimet.edu.ve>
2021-05-10 23:00:02 -0400

    Detalles con el Puerto

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ class App {
 
         this.routes()
 
-        this.app.listen(3000, function() {
+        this.app.listen(process.env.PORT || 3000, function() {
             console.log("Servidor trabajando")
         });
 
